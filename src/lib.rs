654093323e60012f1e5fcc96c9f2a281ac807cee
François Mockers <mockersf@gmail.com>
2019-02-10 02:55:59 +0100

    :sparkles: simple type conversions
    following https://github.com/lightbend/config/blob/master/HOCON.md#automatic-type-conversions

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ mod parser;
 pub mod serde;
 
 /// HOCON document
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Hocon {
     /// A floating value
     Real(f64),
@@ -151,6 +151,7 @@ impl Hocon {
         match *self {
             Hocon::Real(ref v) => Some(*v),
             Hocon::Integer(ref v) => Some(*v as f64),
+            Hocon::String(ref v) => v.parse::<f64>().ok(),
             _ => None,
         }
     }
@@ -159,6 +160,7 @@ impl Hocon {
     pub fn as_i64(&self) -> Option<i64> {
         match *self {
             Hocon::Integer(ref v) => Some(*v),
+            Hocon::String(ref v) => v.parse::<i64>().ok(),
             _ => None,
         }
     }
@@ -179,6 +181,8 @@ impl Hocon {
     pub fn as_bool(&self) -> Option<bool> {
         match *self {
             Hocon::Boolean(ref v) => Some(*v),
+            Hocon::String(ref v) if v == "yes" || v == "true" || v == "on" => Some(true),
+            Hocon::String(ref v) if v == "no" || v == "false" || v == "off" => Some(false),
             _ => None,
         }
     }
@@ -189,282 +193,135 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_string() {
-        let s = r#"{"a":"dndjf"}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_string() {
+        let val = Hocon::String(String::from("test"));
 
-        assert_eq!(doc["a"].as_string().unwrap(), "dndjf");
+        assert_eq!(val.as_bool(), None);
+        assert_eq!(val.as_f64(), None);
+        assert_eq!(val.as_i64(), None);
+        assert_eq!(val.as_string(), Some(String::from("test")));
+        assert_eq!(val[0], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::BadValue);
+
+
     }
 
     #[test]
-    fn parse_int() {
-        let s = r#"{"a":5}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_real() {
+        let val = Hocon::Real(5.6);
 
-        assert_eq!(doc["a"].as_i64().unwrap(), 5);
+        assert_eq!(val.as_bool(), None);
+        assert_eq!(val.as_f64(), Some(5.6));
+        assert_eq!(val.as_i64(), None);
+        assert_eq!(val.as_string(), Some(String::from("5.6")));
+        assert_eq!(val[0], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::BadValue);
     }
 
     #[test]
-    fn parse_float() {
-        let s = r#"{"a":5.7}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_integer() {
+        let val = Hocon::Integer(5);
 
-        assert_eq!(doc["a"].as_f64().unwrap(), 5.7);
+        assert_eq!(val.as_bool(), None);
+        assert_eq!(val.as_f64(), Some(5.0));
+        assert_eq!(val.as_i64(), Some(5));
+        assert_eq!(val.as_string(), Some(String::from("5")));
+        assert_eq!(val[0], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::BadValue);
     }
 
     #[test]
-    fn parse_bool() {
-        let s = r#"{"a":true}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_boolean_false() {
+        let val = Hocon::Boolean(false);
 
-        assert_eq!(doc["a"].as_bool().unwrap(), true);
+        assert_eq!(val.as_bool(), Some(false));
+        assert_eq!(val.as_f64(), None);
+        assert_eq!(val.as_i64(), None);
+        assert_eq!(val.as_string(), Some(String::from("false")));
+        assert_eq!(val[0], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::BadValue);
     }
 
     #[test]
-    fn parse_int_array() {
-        let s = r#"{"a":[5, 6, 7]}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_boolean_true() {
+        let val = Hocon::Boolean(true);
 
-        assert_eq!(doc["a"][1].as_i64().unwrap(), 6);
-        assert_eq!(doc["a"][2].as_i64().unwrap(), 7);
+        assert_eq!(val.as_bool(), Some(true));
+        assert_eq!(val.as_f64(), None);
+        assert_eq!(val.as_i64(), None);
+        assert_eq!(val.as_string(), Some(String::from("true")));
+        assert_eq!(val[0], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::BadValue);
     }
 
     #[test]
-    fn parse_int_array_newline_as_separator() {
-        let s = r#"{"a":[5
-    6
-    ]}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_null() {
+        let val = Hocon::Null;
 
-        assert_eq!(doc["a"][0].as_i64().unwrap(), 5);
-        assert_eq!(doc["a"][1].as_i64().unwrap(), 6);
+        assert_eq!(val.as_bool(), None);
+        assert_eq!(val.as_f64(), None);
+        assert_eq!(val.as_i64(), None);
+        assert_eq!(val.as_string(), None);
+        assert_eq!(val[0], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::BadValue);
     }
 
     #[test]
-    fn parse_object_newline_as_separator() {
-        let s = r#"{"a":5
-"b":6
-}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_bad_value() {
+        let val = Hocon::BadValue;
 
-        assert_eq!(doc["a"].as_i64().unwrap(), 5);
-        assert_eq!(doc["b"].as_i64().unwrap(), 6);
+        assert_eq!(val.as_bool(), None);
+        assert_eq!(val.as_f64(), None);
+        assert_eq!(val.as_i64(), None);
+        assert_eq!(val.as_string(), None);
+        assert_eq!(val[0], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::BadValue);
     }
 
     #[test]
-    fn parse_trailing_commas() {
-        let s = r#"{"a":[5, 6, 7,
-],
-}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_array() {
+        let val = Hocon::Array(vec![Hocon::Integer(5), Hocon::Integer(6)]);
 
-        assert_eq!(doc["a"][1].as_i64().unwrap(), 6);
+        assert_eq!(val.as_bool(), None);
+        assert_eq!(val.as_f64(), None);
+        assert_eq!(val.as_i64(), None);
+        assert_eq!(val.as_string(), None);
+        assert_eq!(val[0], Hocon::Integer(5));
+        assert_eq!(val[1], Hocon::Integer(6));
+        assert_eq!(val[2], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::BadValue);
     }
 
     #[test]
-    fn parse_nested() {
-        let s = r#"{"a":{"b":[{"c":5},{"c":6}]}}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn access_on_hash() {
+        let mut hm = HashMap::new();
+        hm.insert(String::from("a"), Hocon::Integer(5));
+        hm.insert(String::from("b"), Hocon::Integer(6));
+        let val = Hocon::Hash(hm);
 
-        assert_eq!(doc["a"]["b"][1]["c"].as_i64().unwrap(), 6);
+        assert_eq!(val.as_bool(), None);
+        assert_eq!(val.as_f64(), None);
+        assert_eq!(val.as_i64(), None);
+        assert_eq!(val.as_string(), None);
+        assert_eq!(val[0], Hocon::BadValue);
+        assert_eq!(val["a"], Hocon::Integer(5));
+        assert_eq!(val["b"], Hocon::Integer(6));
+        assert_eq!(val["c"], Hocon::BadValue);
     }
 
     #[test]
-    fn parse_newlines() {
-        let s = r#"{"a":
-    5
-    }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
+    fn cast_string() {
+        assert_eq!(Hocon::String(String::from("true")).as_bool(), Some(true));
+        assert_eq!(Hocon::String(String::from("yes")).as_bool(), Some(true));
+        assert_eq!(Hocon::String(String::from("on")).as_bool(), Some(true));
+        assert_eq!(Hocon::String(String::from("false")).as_bool(), Some(false));
+        assert_eq!(Hocon::String(String::from("no")).as_bool(), Some(false));
+        assert_eq!(Hocon::String(String::from("off")).as_bool(), Some(false));
 
-        assert_eq!(doc["a"].as_i64().unwrap(), 5);
-    }
-
-    #[test]
-    fn parse_comment() {
-        let s = r#"{
-    // comment 0
-    "a":5, // comment1
-    "b": 6 # comment 2
-    # comment 3
-    "c": [7 // comment 4
-    # comment 5
-
-    // comment 6
-    8]
-}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["a"].as_i64().unwrap(), 5);
-    }
-
-    #[test]
-    fn parse_keyvalue_separator() {
-        let s = r#"{"a":5,"b"=6,"c" {"a":1}}}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["a"].as_i64().unwrap(), 5);
-        assert_eq!(doc["b"].as_i64().unwrap(), 6);
-        assert_eq!(doc["c"]["a"].as_i64().unwrap(), 1);
-    }
-
-    #[test]
-    fn parse_object_merging() {
-        let s = r#"{
-            "foo" : { "a" : 42 },
-            "foo" : { "b" : 43 }
-        }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["foo"]["a"].as_i64().unwrap(), 42);
-        assert_eq!(doc["foo"]["b"].as_i64().unwrap(), 43);
-    }
-
-    #[test]
-    fn parse_change_type_to_object() {
-        let s = r#"{
-            "foo" : [0, 1, 2],
-            "foo" : { "b" : 43 }
-        }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert!(doc["foo"][0].as_i64().is_none());
-        assert_eq!(doc["foo"]["b"].as_i64().unwrap(), 43);
-    }
-
-    #[test]
-    fn parse_change_type_to_array() {
-        let s = r#"{
-            "foo" : { "b" : 43 },
-            "foo" : [0, 1, 2],
-        }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["foo"][0].as_i64().unwrap(), 0);
-        assert!(doc["foo"]["b"].as_i64().is_none());
-    }
-
-    #[test]
-    fn parse_reset_array_index() {
-        let s = r#"{
-            "foo" : [0, 1, 2],
-            "foo" : [5, 6, 7]
-        }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["foo"][0].as_i64().unwrap(), 5);
-    }
-
-    #[test]
-    fn parse_error() {
-        let s = r#"{
-            "foo" : { "a" : 42 },
-            "foo" : {
-        }"#;
-        let doc = Hocon::load_from_str(s);
-
-        assert!(doc.is_err());
-    }
-
-    #[test]
-    fn wrong_index() {
-        let s = r#"{ "a" : 42 }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        if let Hocon::BadValue = doc["missing"] {
-
-        } else {
-            assert!(false);
-        }
-        if let Hocon::BadValue = doc[0] {
-
-        } else {
-            assert!(false);
-        }
-    }
-
-    #[test]
-    fn wrong_casts() {
-        let s = r#"{ "a" : 42 }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert!(doc["missing"].as_i64().is_none());
-        assert!(doc["missing"].as_f64().is_none());
-        assert!(doc["missing"].as_bool().is_none());
-        assert!(doc["missing"].as_string().is_none());
-    }
-
-    #[test]
-    fn parse_root_braces_omitted() {
-        let s = r#""foo" : { "b" : 43 }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["foo"]["b"].as_i64().unwrap(), 43);
-    }
-
-    #[test]
-    fn parse_unquoted_string() {
-        let s = r#"{"foo" : { b : hello }}"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["foo"]["b"].as_string().unwrap(), "hello");
-    }
-
-    #[test]
-    fn parse_path() {
-        let s = r#"{foo.b : hello }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["foo"]["b"].as_string().unwrap(), "hello");
-    }
-
-    #[test]
-    fn parse_concat() {
-        let s = r#"{"foo" : "hello"" world n°"1 }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["foo"].as_string().unwrap(), "hello world n°1");
-    }
-
-    #[test]
-    fn parse_path_substitution() {
-        let s = r#"{"who" : "world", "number": 1, "bar": "hello "${who}" n°"${number} }"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["bar"].as_string().unwrap(), "hello world n°1");
-    }
-
-    #[test]
-    fn parse_file_ends_with_unquoted_string() {
-        let s = r#"#
-foo:bar"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["foo"].as_string().unwrap(), "bar");
-    }
-
-    #[test]
-    fn parse_comment_in_array_no_comma() {
-        let s = r#"a=[1 // zut
-        2]"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["a"][0].as_i64().unwrap(), 1);
-    }
-
-    #[test]
-    fn parse_substitute_array() {
-        let s = r#"a=[1, 2, 3],b=[${a}]"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["b"][0][1].as_i64().unwrap(), 2);
-    }
-
-    #[test]
-    fn parse_empty_objects() {
-        let s = r#"a={b{}},b=5"#;
-        let doc = dbg!(Hocon::load_from_str(s).unwrap());
-
-        assert_eq!(doc["b"].as_i64().unwrap(), 5);
+        assert_eq!(Hocon::String(String::from("5.6")).as_f64(), Some(5.6));
+        assert_eq!(Hocon::String(String::from("5.6")).as_i64(), None);
+        assert_eq!(Hocon::String(String::from("5")).as_f64(), Some(5.0));
+        assert_eq!(Hocon::String(String::from("5")).as_i64(), Some(5));
     }
 
 }
